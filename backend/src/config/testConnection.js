37f4d770c1013508cbfea3676b75e32123d04522
valienteCDV <valiente40@gmail.com>
2024-08-28 16:49:58 -0300

    tratando de conectar con postgres

---
+++ backend/src/config/testConnection.js
@@ -0,0 +1,12 @@
+const sequelize = require('./database');
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexión establecida correctamente.');
+  } catch (error) {
+    console.error('No se pudo conectar a la base de datos:', error);
+  }
+}
+
+testConnection();
