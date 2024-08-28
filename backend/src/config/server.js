dfa923eab50595c5f20a53cd589f737d654b6102
valienteCDV <valiente40@gmail.com>
2024-08-28 14:50:10 -0300

    se crea la estructura de carpetas, empezaremos con la configuracion de la base de datos

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,5 +1,5 @@
-const app = require('../app');
-const { PORT } = process.env;
+const app = require('../app'); // Nota: Cambié './app' a '../app'
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
